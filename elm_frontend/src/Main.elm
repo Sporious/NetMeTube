b84886a69342cc2d1d2c0ffaced97a364627fc61
Kieran McKenzie <sporious@googlemail.com>
2020-05-24 16:24:24 +0100

    init

--- elm_frontend/src/Main.elm
+++ elm_frontend/src/Main.elm
@@ -1,66 +1,96 @@
 module Main exposing (main)
 
 import Browser exposing (Document)
-import Element exposing (alignLeft, el, height, image, padding, paragraph, px, rgb, rgb255, row, spacing, text, width)
+import Browser.Navigation as Nav
+import Element exposing (alignLeft, el, fill, height, image, padding, paragraph, px, rgb, rgb255, row, spacing, text, width)
 import Element.Background as Background
 import Element.Border as Border exposing (color)
+import Element.Events exposing (onClick)
+import Task
+import Time
+import Url
 
 
 type alias Model =
     { a : String
+    , zone : Time.Zone
+    , time : Time.Posix
+    , key : Nav.Key
+    ,url : Url.Url
     }
 
 
 type Msg
-    = HomePressed
-    | ProfilePressed
-    | LogoutPressed
-    | NoOp
+    = NoOp
+    | Tick Time.Posix
+    | AdjustTimeZone Time.Zone
+    | HomeClicked
+    | ProfileClicked
+    | LogoutClicked
+    | UrlChanged Url.Url
+
 
 
 main : Program () Model Msg
 main =
-    Browser.application { init = init, view = view, update = update, subscriptions = subscriptions, onUrlRequest = onUrlRequest, onUrlChange = onUrlChange }
+    Browser.application { init = init, view = view, update = update, subscriptions = subscriptions, onUrlRequest = onUrlRequest, onUrlChange = UrlChanged }
 
 
 view : Model -> Document Msg
 view model =
-    { title = "Page title", body = [ Element.layout [] <| title_bar ] }
+    { title = "Page title", body = [ Element.layout [] <| title_bar model ] }
 
 
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        Tick newTime ->
+            ( { model | time = newTime }, Cmd.none )
+
+        AdjustTimeZone zone ->
+            ( { model | zone = zone }, Cmd.none )
+
+        HomeClicked -> (model,  Nav.pushUrl model.key  <| Url.toString model.url ++ "extra chars")
+        ProfileClicked -> (model, Cmd.none)
+        LogoutClicked -> (model, Cmd.none)
+        UrlChanged url -> ( { model | url = url, a = model.a ++ "1" }   , Cmd.none)
+
 
 
 subscriptions model =
-    Sub.none
+    Time.every 1000 Tick
 
 
 onUrlRequest request =
     NoOp
 
 
-onUrlChange url =
-    NoOp
 
 
+init : () -> Url.Url -> Nav.Key -> (Model, Cmd Msg)
 init flags url key =
-    ( { a = "aval" }, Cmd.none )
+    ( { a = "Test text", zone = Time.utc, time = Time.millisToPosix 0, key = key, url = url }, Task.perform AdjustTimeZone Time.here )
 
 
-title_bar =
+title_bar model =
     row
         [ alignLeft
-        , padding 10
-        , spacing 10
+        , padding 20
+        , spacing 20
         , Background.color <| rgb 10 10 10
         , Border.solid
         , Border.width <| 1
         , Border.rounded 10
         , Border.shadow { offset = ( 10, 10 ), size = 3, blur = 0, color = rgb255 0 0 10 }
+
         ]
         [ image [ width <| px 50, height <| px 50 ] { description = "image_desc", src = "https://avatars0.githubusercontent.com/u/285019?s=460&u=11f599c6bf0717819b628163dca450240bcdba62&v=4" }
-        , paragraph [] [ text "Home" ]
-        , paragraph [] [ text "Profile" ]
-        , paragraph [] [ text "Logout" ]
+        , paragraph [onClick HomeClicked ] [ text "Home" ]
+        , paragraph [onClick ProfileClicked] [ text "Profile" ]
+        , paragraph [onClick LogoutClicked] [ text "Logout" ]
+        , text <| String.fromInt (Time.toSecond model.zone model.time)
+        , text model.a
         ]
---
+++ yoproj/README.md
@@ -0,0 +1,31 @@
+# yoproj
+
+## Development
+
+Start the hot-reloading webpack dev server:
+
+    npm start
+
+Navigate to <http://localhost:3000>.
+
+Any changes you make to your files (.elm, .js, .css, etc.) will trigger
+a hot reload.
+
+## Production
+
+When you're ready to deploy:
+
+    npm run build
+
+This will create a `dist` folder:
+
+    .
+    ├── dist
+    │   ├── index.html
+    │   ├── main-60b725f10c9c85c70d97.css
+    │   └── main-5df766af1ced8ff1fe0a.js
+
+Serve it locally to sanity-check the results:
+
+    npm install -g serve
+    serve dist
