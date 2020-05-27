aa86e592926940ffe1f6e04ae8b8981ada240fe2
Iksf <sporious@googlemail.com>
2020-05-27 05:49:12 +0100

    login box
    Signed-off-by: Iksf <sporious@googlemail.com>

--- elm_frontend/src/Main.elm
+++ elm_frontend/src/Main.elm
@@ -2,29 +2,30 @@ module Main exposing (main)
 
 import Browser exposing (Document)
 import Browser.Navigation as Nav
-import Element exposing (alignLeft, centerX, el, fill, height, image, padding, paragraph, px, rgb, rgb255, row, spacing, text, width)
+import Element exposing (Color, alignLeft, centerX, el, fill, height, image, padding, paragraph, px, rgb, rgb255, row, spacing, text, width)
 import Element.Background as Background
 import Element.Border as Border exposing (color)
 import Element.Events exposing (onClick)
 import Element.Input exposing (button)
+import Html
+import LoginBox
+import Random
+import Result exposing (andThen)
 import Task
 import Time
 import Url
 
 
 type alias Model =
-    {
-     zone : Time.Zone
+    { zone : Time.Zone
     , time : Time.Posix
     , key : Nav.Key
     , url : Url.Url
-    , main_model: MainModel
+    , a : String
+    , colour : Color
+    , login_model : LoginBox.Model
     }
 
-type alias MainModel =
-    {
-    a : String
-    }
 
 type Msg
     = NoOp
@@ -34,8 +35,9 @@ type Msg
     | ProfileClicked
     | LogoutClicked
     | UrlChanged Url.Url
-
-
+    | TriggerNewRandomColours Time.Posix
+    | ApplyColours ( Int, Int, Int )
+    | LoginMsg LoginBox.Msg
 
 
 main : Program () Model Msg
@@ -54,37 +56,64 @@ view model =
                 , centerX
                 ]
                 [ title_bar model
+                , Element.map LoginMsg (LoginBox.view model.login_model)
                 ]
         ]
     }
 
 
+updateWith : (subModel -> Model) -> (subMsg -> Msg) -> Model -> ( subModel, Cmd subMsg ) -> ( Model, Cmd Msg )
+updateWith toModel toMsg model ( subModel, subCmd ) =
+    ( toModel subModel
+    , Cmd.map toMsg subCmd
+    )
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        NoOp ->
+    case ( msg, model ) of
+        ( NoOp, _ ) ->
             ( model, Cmd.none )
 
-        Tick newTime ->
+        ( Tick newTime, _ ) ->
             ( { model | time = newTime }, Cmd.none )
 
-        AdjustTimeZone zone ->
+        ( AdjustTimeZone zone, _ ) ->
             ( { model | zone = zone }, Cmd.none )
 
-        HomeClicked ->
+        ( HomeClicked, _ ) ->
             ( model, Nav.pushUrl model.key <| Url.toString model.url ++ "extra chars" )
 
-        ProfileClicked ->
+        ( ProfileClicked, _ ) ->
             ( model, Cmd.none )
 
-        LogoutClicked ->
+        ( LogoutClicked, _ ) ->
             ( model, Cmd.none )
 
-        UrlChanged url ->
+        ( UrlChanged url, _ ) ->
             ( model, Cmd.none )
+
+        ( TriggerNewRandomColours time, _ ) ->
+            ( model, Random.generate ApplyColours colour_generator )
+
+        ( ApplyColours ( r, g, b ), _ ) ->
+            ( { model | colour = rgb255 r g b }, Cmd.none )
+
+        ( LoginMsg subMsg, _ ) ->
+            let
+                ( login_model, subCmd ) =
+                    LoginBox.update subMsg model.login_model
+            in
+            ( { model | login_model = login_model }, Cmd.map LoginMsg subCmd )
+
 
 subscriptions model =
-    Time.every 1000 Tick
+    Sub.batch [ Time.every 1000 Tick, Time.every 500 TriggerNewRandomColours ]
+
+
+colour_generator : Random.Generator ( Int, Int, Int )
+colour_generator =
+    Random.map3 (\a b c -> ( a, b, c )) (Random.int 0 255) (Random.int 0 255) (Random.int 0 255)
 
 
 onUrlRequest request =
@@ -93,7 +122,7 @@ onUrlRequest request =
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
-    ( {  zone = Time.utc, time = Time.millisToPosix 0, key = key, url = url, main_model = {a =  ""} }, Task.perform AdjustTimeZone Time.here )
+    ( { zone = Time.utc, time = Time.millisToPosix 0, key = key, url = url, a = "", colour = rgb255 10 10 10, login_model = LoginBox.init }, Task.perform AdjustTimeZone Time.here )
 
 
 title_bar model =
@@ -103,14 +132,16 @@ title_bar model =
         , spacing 20
         , Background.color <| rgb 10 10 10
         , Border.solid
-        , Border.width <| 1
+        , Border.width 1
         , Border.rounded 10
-        , Border.shadow { offset = ( 10, 10 ), size = 3, blur = 0, color = rgb255 0 0 10 }
+
+        -- , Border.shadow { offset = ( 10, 10 ), size = 3, blur = 0, color = rgb255 0 0 10 }
+        , Border.glow model.colour 5
         ]
         [ image [ width <| px 50, height <| px 50 ] { description = "image_desc", src = "https://avatars0.githubusercontent.com/u/285019?s=460&u=11f599c6bf0717819b628163dca450240bcdba62&v=4" }
         , paragraph [ onClick HomeClicked ] [ text "Home" ]
         , paragraph [ onClick ProfileClicked ] [ text "Profile" ]
         , button [] { onPress = Just LogoutClicked, label = text "Logout" }
-        , text <| String.fromInt (Time.toSecond model.zone model.time)
-        , text model.main_model.a
+        , text <| String.fromInt <| Time.toSecond model.zone model.time
+        , text model.a
         ]
